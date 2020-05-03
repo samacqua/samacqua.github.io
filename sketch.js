ecd531d502e3bdaadddfb5cfe8730beff8839c41
Sam Acquaviva <samacqua@gmail.com>
2020-05-03 12:56:16 -0400

    Adding theme adjusted background animation

--- sketch.js
+++ sketch.js
@@ -4,17 +4,17 @@ new p5(function(p) {
   
     p.setup = function() {
       var canvas = p.createCanvas(p.windowWidth, p.windowHeight);
-      p.stroke(p.color('#ce93d8'), 120);
+      p.stroke(p.color('#4b88b0'), 120);
       p.noFill();
       p.frameRate(30);
     }
   
     p.draw = function() {
-      p.background(255, 15);
+      p.background(50, 15);
       p.beginShape();
       for (var i = -1; i <= d; i++) {
         var x = p.map(i, 0, d, 0, p.width);
-        var y = p.map(p.noise(i * 0.01, t * 0.005), 0, 1, 0, p.height) + p.map(i, 0, d, -p.height/4, p.height/2);
+        var y = p.map(p.noise(i * 0.01, t * 0.006), 0, 1, 0, p.height) + p.map(i, 0, d, -p.height/4, p.height/2);
         p.curveVertex(x, y);
       }
       p.endShape(p.OPEN);
